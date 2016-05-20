21c59e3f6ac576b4900bf14cc64d2cf27e02f0a6
JacksonBates <malgalin@gmail.com>
2016-05-20 17:06:13 +1000

    added simple express response to index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var mongodb = require('mongodb');
-
+var express = require('express')
 var MongoClient = mongodb.MongoClient;
 var url = process.env.URL_SHORT_MONGOLAB_URI
 
+var app = express()
 
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function(request, response) {
+  response.send('App is running')
+})
 
 MongoClient.connect(url, function (err, db) {
   if (err) {
@@ -17,3 +23,7 @@ MongoClient.connect(url, function (err, db) {
     db.close();
   }
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port ', app.get('port'))
+})
